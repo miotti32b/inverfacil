36de82ce40c28d994dbb4d131ac96f82343b06c0
miotti32b <102973744+miotti32b@users.noreply.github.com>
2024-09-09 10:44:36 -0300

    edicion calculardolar

--- static/js/animaciones.js
+++ static/js/animaciones.js
@@ -1,14 +1,7 @@
-// Detecta cuando el usuario hace scroll en dispositivos móviles
-window.addEventListener('scroll', function() {
-  if (window.innerWidth <= 768) {
-      document.body.classList.add('scrolled');
-  }
+document.querySelector('button').addEventListener('click', function() {
+  const calculadora = document.querySelector('.calculadora-container');
+  calculadora.classList.add('show');
 });
 
-document.addEventListener('mousemove', function(e) {
-  const focusCircle = document.getElementById('focus-circle');
-  focusCircle.style.left = `${e.pageX - 100}px`; // Centra el círculo horizontalmente
-  focusCircle.style.top = `${e.pageY - 100}px`; // Centra el círculo verticalmente
-});
 
 
