0cd4bf63f30ae6827d4acf6c5c7e48ecd94e7fa7
miotti32b <102973744+miotti32b@users.noreply.github.com>
2024-09-14 17:59:00 -0300

    actualizad

--- mi_sitio_web/settings.py
+++ mi_sitio_web/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.invertiresfacil.com', 'invertiresfacil.com']
 
