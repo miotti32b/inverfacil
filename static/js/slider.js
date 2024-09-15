af77d7f1c17c63d9b50696cb23caed384bd4f07f
miotti32b <102973744+miotti32b@users.noreply.github.com>
2024-09-15 20:48:55 -0300

    version

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,16 +1,7 @@
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
-
-function showNextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-    const offset = -currentSlide * 100;
-    document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
-}
-
-// Cambia de imagen automáticamente cada 2.5 segundos
-setInterval(showNextSlide, 2500);
+function toggleMenu() {
+    const menu = document.getElementById('menu-desplegable');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+  }
 
 
 
