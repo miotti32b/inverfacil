918c66de9dfd57747ee5391fecf90726d0a2194f
miotti32b <miotti322@gmail.com>
2025-05-19 16:48:38 -0300

    Activar WhiteNoise en wsgi para servir /static en producción

--- mi_sitio_web/wsgi.py
+++ mi_sitio_web/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_sitio_web.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
