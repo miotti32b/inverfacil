510302863476aa82cd99494d07da78c69bfa1341
miotti32b <miotti322@gmail.com>
2025-06-29 18:26:26 -0300

    Register Question and Option models in admin for quiz management

--- calculadora/admin.py
+++ calculadora/admin.py
@@ -6,8 +6,11 @@ admin.site.register(CarreraRata)
 
 
 from django.contrib import admin
-from .models import Player, PlayerResult, Scenario  # importa tus modelos
+from .models import Player, PlayerResult, Scenario, Question, Option  # importa tus modelos
 
 admin.site.register(Player)
 admin.site.register(PlayerResult)
 admin.site.register(Scenario)
+
+admin.site.register(Question)
+admin.site.register(Option)
