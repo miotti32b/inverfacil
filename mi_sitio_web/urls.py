bd34a73b82a0c0e30f2d3b3b2b7ef5eb434627aa
miotti32b <miotti322@gmail.com>
2025-06-29 17:03:32 -0300

    Fix: add missing include import for allauth URLs

--- mi_sitio_web/urls.py
+++ mi_sitio_web/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
 from calculadora import views
 from calculadora.views import api_endpoint
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
