0cfebd1e0d67d477da1859ad4b8faf87b949a0de
miotti32b <miotti322@gmail.com>
2025-06-29 17:59:18 -0300

    Fix: import calculadora_views in urls.py

--- mi_sitio_web/urls.py
+++ mi_sitio_web/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include
+from calculadora import views as calculadora_views
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+
 
 def create_superuser(request):
     if not User.objects.filter(username='admin').exists():
