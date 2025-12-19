b8c0adcd89ec1f25e9d04a8eb32360e44320ae21
miotti32b <miotti322@gmail.com>
2025-12-18 21:43:09 -0300

    ahora ffgiffffsda

--- calculadora/management/commands/seed_planes.py
+++
@@ -1,6 +0,0 @@
-class Plan(models.Model):
-    nombre = models.CharField(max_length=200)
-    precio = models.IntegerField()
-
-    def __str__(self):
-        return self.nombre
--- calculadora/models.py
+++ calculadora/models.py
@@ -138,8 +138,7 @@ class DiagnosticoFinanciero(models.Model):
 class Plan(models.Model):
     nombre = models.CharField(max_length=200)
     precio = models.IntegerField()
-    preference_id = models.CharField(max_length=255, blank=True, null=True)
-
+    preference_id = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
